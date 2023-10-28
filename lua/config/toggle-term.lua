9b1bfa3cf771b9552a1e0e21f292f9a0dc99ca00
bili9blood <3501202786@qq.com>
2023-10-28 11:07:31 +0800

    update

--- lua/config/toggle-term.lua
+++ lua/config/toggle-term.lua
@@ -1,8 +1,24 @@
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function()
+    vim.cmd("startinsert!")
+  end,
+})
 
 function _Lazygit_toggle()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _Lazygit_toggle()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>tg", "<cmd>lua _Lazygit_toggle()<CR>", {noremap = true, silent = true})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -36,6 +36,67 @@ require("lazy").setup({
     end
   },
 
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end,
+    keys = { { "gc", mode = { "n", "v" } }, { "gb", mode = { "n", "v" } } },
+    event = "User FileOpened",
+  },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+    end,
+    event = "User FileOpened",
+    cmd = "Gitsigns",
+  },
+
+  {
+      'windwp/nvim-autopairs',
+      event = "InsertEnter",
+      opts = {}
+  },
+
+  {
+    "b0o/schemastore.nvim",
+    lazy = true,
+  },
+
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      lsp = {
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" }
+  },
+
+  {
+    "hiphish/rainbow-delimiters.nvim",
+    event = "User FileOpened",
+  },
+
+  {
+    "SmiteshP/nvim-navic",
+    event = "User FileOpened",
+  },
+
   "folke/which-key.nvim",
 
   {
